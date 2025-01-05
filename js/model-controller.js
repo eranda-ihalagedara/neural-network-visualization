7acd232e299be25a59a355bfec847bea5504ae4b
Eranda Ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2025-01-05 20:23:03 +1100

    Implement intermediate layer output model

--- js/model-controller.js
+++ js/model-controller.js
@@ -27,5 +27,37 @@ export class ModelController {
 
         output.innerText = pred;
     }
+
+    compileIntermediateLayerModel(model) {
+        this.intermediateModel = tf.model({
+            inputs: this.model.inputs,
+            outputs: this.model.layers.map(layer => layer.output)
+          });
+    }
+
+    async predictIntermediateLayerOutputs(ctxImage, output) {
+        if (!this.intermediateModel) {
+            console.log('Intermediate Model not loaded');
+            return;
+        }
+        
+        // const ctxImage = ctx.getImageData(0, 0, canvas.height, canvas.width);
+        const inputImg = tf.tidy(() => {
+            const image = tf.browser.fromPixels(ctxImage, 1);
+            const reImg = image.resizeBilinear([28,28]).toFloat().div(tf.scalar(255));
+            return tf.expandDims(reImg, 0);
+        });
+
+        const pred = tf.tidy(() => {
+            const preds = this.intermediateModel.predict(inputImg);
+            return preds;
+        });
+
+        const layerActivations = pred.map((out)=>{ return out.squeeze().arraySync();});
+        const layerShapes = pred.map((out)=>{return out.squeeze().shape;});
+
+        return {layerActivations, layerShapes};
+
+    }
 }
 
